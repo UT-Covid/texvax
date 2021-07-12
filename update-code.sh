61ba6355cba7becaca8efcb163eca2b4ee856a0f
Maytal Dahan <maytaldahan@gmail.com>
2021-07-12 12:38:03 -0700

    update code script/instructions

---
+++ update-code.sh
@@ -0,0 +1,8 @@
+
+Log in to covid19-stage@cic02.tacc.utexas.edu
+> ssh -i ~/.ssh/id_covid_staging_priv covid19-stage@cic02.tacc.utexas.edu
+Navigate to the /home/covid19-stage/texvax-serve-deploy directory
+> cd /home/covid19-stage/texvax-serve-deploy
+Run the ./restart-dev or ./restart-prod scripts
+> ./restart-dev
+> ./restart-prod
